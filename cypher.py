67c29b96aea6a63e3fbda237eafe64093eccae72
Adam Ruszkowski <adam.ruszkowski@gmail.com>
2019-09-26 18:35:12 +0100

    Rework anonymisation into separate class

---
+++ cypher.py
@@ -0,0 +1,53 @@
+from random import SystemRandom
+
+# Valid numberplate characters - A-Z, 1-9
+VALID_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+
+class NumberplateCypher:
+    """A cypher to anonymise numberplates.
+    """
+    def __init__(self):
+        """Initialises the cypher, including the set-up of character lookups.
+        """
+        self._cyphers = [VALID_CHARS for _ in range(7)]
+        self.randomise_cyphers()
+
+    @staticmethod
+    def _shuffle(seq):
+        rng = SystemRandom()
+        return sorted(seq, key=lambda _: rng.random())
+
+    def randomise_cyphers(self):
+        """Randomises the cyphers to allow for a new equivalence to be made,
+        e.g. if two data sets may contain the same numberplate and the end-user
+        should not be able to connect the two together, the cyphers should be
+        shuffled between processing the two data sets to ensure this should
+        not be the case.
+        """
+        self._cyphers = [self._shuffle(c) for c in self._cyphers]
+
+    def anonymise(self, plate):
+        """Anonymises a provided numberplate in the following manner:
+        - Any spaces are removed from the plate
+        - Characters that are not A-Z or 0-9 are ignored
+        - Only the first 7 non-space characters are anonymised
+
+        :param plate: the numberplate to be anonymised
+        :type plate: str
+        :return: the anonymised numberplate (contains no spaces)
+        :rtype: str
+        """
+        no_spaces = plate.upper().replace(' ', '')
+        new_plate = []
+        for i, c in enumerate(no_spaces[:7]):
+            if c in VALID_CHARS:
+                char_index = VALID_CHARS.index(c)
+                new_plate.append(self._cyphers[i][char_index])
+        return ''.join(new_plate)
+
+
+if __name__ == '__main__':
+    nc = NumberplateCypher()
+    print(nc.anonymise('P584 PVT'))
+    help(NumberplateCypher)
--- main.py
+++ main.py
@@ -1,49 +1,15 @@
 import sys
-import pandas as pd
-import numpy as np
 import tkinter
 import pickle
 import pyperclip
 from PyQt5 import QtWidgets, uic
 from tkinter import messagebox
 
+from cypher import NumberplateCypher
 import exe_fix
 
-# ////////////////////////////////////////////////////////////////
-# Define function to import external files when using PyInstaller.
-# ////////////////////////////////////////////////////////////////
-# def resource_path(relative_path):
-#     """ Get absolute path to resource, works for dev and for PyInstaller """
-#     try:
-#         # PyInstaller creates a temp folder and stores path in _MEIPASS
-#         base_path = sys._MEI14482
-#     except Exception:
-#         base_path = os.path.abspath(".")
-#
-#     return os.path.join(base_path, relative_path)
 
 qtCreatorFile = "gui.ui" # Enter xml file here.
-characters  = {'Chr': ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
-               'R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']}
-key = pd.DataFrame(characters)
-
-cyphers = [key]
-
-def cypher():
-        for i in range(1,8): # make a list of 7 cyphers (one for each licence plate character by randomly arranging characters A-9
-            cyphers.append(key)
-            cyphers[i]['Values'] = np.random.random_sample(len(cyphers[0]))
-            cyphers[i] = cyphers[i].sort_values('Values')
-            cyphers[i] = cyphers[i].reset_index(drop=True)
-
-        with open("cyphers.bin", "wb") as output:
-            pickle.dump(cyphers, output)
-
-try:
-    with open("cyphers.bin", "rb") as data:
-        cyphers = pickle.load(data)
-except FileNotFoundError:
-    cypher()
 
 parent = tkinter.Tk()
 parent.overrideredirect(1) # Avoid it appearing and then disappearing quickly
@@ -51,13 +17,11 @@ parent.withdraw() # Hide the window
 
 class Ui(QtWidgets.QDialog):
     def __init__(self):
-        super(Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('gui.ui', self) # Load the .ui file
-        # //////////////////////////////////////////
-        # Load the .ui file when using PyInstaller
-        # //////////////////////////////////////////
-        # interface = resource_path("gui.ui")
-        # uic.loadUi(interface, self)
+        super(Ui, self).__init__()
+
+        self.cypher = NumberplateCypher()
+
+        uic.loadUi('gui.ui', self)
 
         self.encrypt_button = self.findChild(QtWidgets.QPushButton, 'encrypt_button')
         self.encrypt_button.clicked.connect(self.encrypt) # Remember to pass the definition/method, not the return value!
@@ -71,32 +35,16 @@ class Ui(QtWidgets.QDialog):
         self.show() # Show the GUI
 
     def encrypt(self):
-        try:
-            plate_no = self.input.toPlainText()
-            no_list = plate_no.splitlines()
-            result_list = []
-            output = ''
-            for no in no_list:
-                if no != '':
-                    chrlist = list(no)
-                    for i in range(0,7):
-                        index = cyphers[0].index[cyphers[0]['Chr'] == chrlist[i]] # get index of the plate no character in A-9 range
-                        index = index[0]
-                        chrlist[i] = cyphers[i+1].at[index, 'Chr'] # replace character in plate no with corresponding character in corresponding cypher
-                    result_list.append(chrlist)
-            for r in result_list:
-                output = output + ''.join(r) + '\n'
-            self.output.setText(output)
-            pyperclip.copy(output)
-            info = messagebox.showinfo('Encrypted', 'Copied to clipboard', parent=parent)
-        except IndexError:
-            self.output.setText('Please enter a set of 7 character number plates without spaces e.g. \nBD51SMR \n6TGZ044 \nOR68ASM ')
+        plates = self.input.toPlainText().splitlines()
+        anonymised = '\n'.join([self.cypher.anonymise(plate) for plate in plates])
+        self.output.setText(anonymised)
+        pyperclip.copy(anonymised)
+        info = messagebox.showinfo('Encrypted', 'Copied to clipboard', parent=parent)
 
     def refresh(self):
         okcancel = messagebox.askokcancel('Refresh Cypher', 'Are you sure you want to refresh the cypher?', parent=parent) # OK / Cancel
         if okcancel:
-            cyphers = [key]
-            cypher()
+            self.cypher.randomise_cyphers()
             self.output.setText('')
             self.input.setText('')
 
