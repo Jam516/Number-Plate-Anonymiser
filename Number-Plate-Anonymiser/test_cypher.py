e662de12742214892b22f6724fb07ce136bcd1a3
Adam Ruszkowski <adam.ruszkowski@gmail.com>
2019-10-23 09:18:14 +0100

    Add basic cypher tests

---
+++ Number-Plate-Anonymiser/test_cypher.py
@@ -0,0 +1,22 @@
+from cypher import NumberplateCypher
+
+
+class TestNumberplateCypher:
+    plate = 'P584PVT'
+    cypher = NumberplateCypher()
+
+    def test_anonymisation(self):
+        TestNumberplateCypher.first_shuffle = TestNumberplateCypher.cypher.anonymise(
+            TestNumberplateCypher.plate
+        )
+
+        assert TestNumberplateCypher.first_shuffle != TestNumberplateCypher.plate
+
+    def test_randomising(self):
+        TestNumberplateCypher.cypher.randomise_cyphers()
+
+        second_shuffle = TestNumberplateCypher.cypher.anonymise(
+            TestNumberplateCypher.plate
+        )
+
+        assert second_shuffle != TestNumberplateCypher.first_shuffle
